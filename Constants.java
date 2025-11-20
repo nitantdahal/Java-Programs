658770ac98079c8d173166189dce24ec97204e28
nsdhl <nitantdahal123@gmail.com>
2025-11-20 16:07:12 +0545

    Use of final keyword (Constants)

---
+++ Constants.java
@@ -0,0 +1,11 @@
+public class Constants {
+    public static void main(String[] args) {
+        int x = 5;
+        x = 10; // x value updated to 10
+        System.out.println(x); // prints 10
+
+        final int a = 10; // final keyword make value constant within the program
+        a = 2; // Value not updated to 2 (Shows error)
+        System.out.println(a);
+    }
+}
