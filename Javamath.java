70d53d27007a6bf8f3666c8b16d8993bd93ed9e1
nsdhl <nitantdahal123@gmail.com>
2025-11-23 21:48:43 +0545

    Math functions

---
+++ Javamath.java
@@ -0,0 +1,13 @@
+public class Javamath {
+    public static void main(String[] args) {
+        System.out.println(Math.max(5, 10));
+        System.out.println(Math.min(5, 10));
+        System.out.println(Math.sqrt(10));
+        System.out.println(Math.abs(-5.11));
+        System.out.println(Math.pow(5, 10));
+        System.out.println(Math.round(5.10));
+        System.out.println(Math.ceil(4.7));
+        System.out.println(Math.floor(4.7));
+        System.out.println(Math.random());
+    }
+}
