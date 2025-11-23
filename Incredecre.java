3d74c3b232902392c6ed11c6e2b6d0cc545cec12
nsdhl <nitantdahal123@gmail.com>
2025-11-23 12:44:46 +0545

    Increment Decrement operation

---
+++ Incredecre.java
@@ -0,0 +1,13 @@
+public class Incredecre {
+    public static void main(String[] args) {
+        int x = 2;
+        ++x;
+        System.out.println(x);
+        x++;
+        System.out.println(x);
+        x--;
+        System.out.println(x);
+        --x;
+        System.out.println(x);
+    }
+}
